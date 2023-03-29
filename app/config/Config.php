62485bde11b62e49fa13320cc64b5323c582366f
RensSevink <332726@student.mboutrecht.nl>
2023-03-29 14:15:34 +0200

    setup project

---
+++ app/config/Config.php
@@ -0,0 +1,9 @@
+<?php 
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'examendag3');
+
+    define('APPROOT', dirname(dirname(__FILE__)));
+    define('URLROOT', 'http://examenproefdag.com');
+    define('SITENAME', 'MVC Framework');
